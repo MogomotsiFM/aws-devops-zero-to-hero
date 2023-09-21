0d4e6414feabb97669adb14aeab8412e87055796
MogomotsiFM <113692617+MogomotsiFM@users.noreply.github.com>
2023-09-22 00:37:24 +0200

    Modified the script to pull and run the application container.

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling the Docker image from Docker Hub"
+echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
+# docker build -t "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_PROFILE_NAME/simple-python-flask-app:latest" .
+docker pull "mogomotsi/simple-python-flask-app:latest"  
 
 # Run the Docker image as a container
-echo
+echo "Starting the Docker container"
+docker run -p "mogomotsi/simple-python-flask-app"
