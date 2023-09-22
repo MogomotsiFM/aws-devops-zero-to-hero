f1c7480e0200fc413d79eef78383b10d7614f3dc
MogomotsiFM <113692617+MogomotsiFM@users.noreply.github.com>
2023-09-22 15:02:52 +0200

    Fix

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,7 @@ set -e
 # Stop the running container (if any)
 echo "Stoping all running containers. There is only one anyways."
 containerId=`sudo docker ps | awk -F " " '{print $1}' | tail -1`
-if [ $containerId != "CONTAINER" ]; then
+if [[ $containerId != "CONTAINER" ]]; then
     sudo docker stop $containerId
 fi
 # docker stop $(docker ps -q -f "name=$1")
