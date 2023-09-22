7477cabd02f08fb9125eab2c2fd399a1b1784630
MogomotsiFM <113692617+MogomotsiFM@users.noreply.github.com>
2023-09-22 03:19:37 +0200

    Delete the container if it is running.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,6 +3,9 @@ set -e
 
 # Stop the running container (if any)
 echo "Stoping all running containers. There is only one anyways."
-docker stop `docker ps -q`
+containerId = `docker ps | awk -F " " '{print $1}'`
+if [ ! $containerId == "" ]; then
+    docker stop $containerId
+fi
 # docker stop $(docker ps -q -f "name=$1")
 sleep 30
