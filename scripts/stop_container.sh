09af8366eb65fa8e0a130d5ded20003726423fec
MogomotsiFM <113692617+MogomotsiFM@users.noreply.github.com>
2023-09-22 03:56:43 +0200

    Fixed a bug in the condition that checks if a container is running.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,7 @@ set -e
 # Stop the running container (if any)
 echo "Stoping all running containers. There is only one anyways."
 containerId = `docker ps | awk -F " " '{print $1}'`
-if [ ! $containerId == "" ]; then
+if [ $containerId != "CONTAINER" ]; then
     docker stop $containerId
 fi
 # docker stop $(docker ps -q -f "name=$1")
